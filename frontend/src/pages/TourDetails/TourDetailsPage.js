eb1a6c04ece4fc4d730cf33514863d18b422242c
doTruongGiang-IT <dotruonggiang290@gmail.com>
2021-10-21 18:06:35 +0700

    [Add]: Tour details screen

--- frontend/src/pages/TourDetails/TourDetailsPage.js
+++ frontend/src/pages/TourDetails/TourDetailsPage.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import TourDetailsForm from '../../components/TourDeatailsForm/TourDetailsForm';
+import { useParams } from 'react-router';
+import { selectTourDetails, getTourDetails } from '../../features/tour/tourSlice';
+import { useSelector, useDispatch } from 'react-redux';
 
 const TourDetailsPage = () => {
+    let {id} = useParams();
+    const dispatch = useDispatch();
+    const tourDetails = useSelector(selectTourDetails);
+
+    useEffect(() => {
+        dispatch(getTourDetails(id));
+    }, [dispatch, id]);
+
     return (
-        <div>
-            Tour Details
-        </div>
+        <TourDetailsForm />
     )
 }
 
