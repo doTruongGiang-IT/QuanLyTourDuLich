7a2e663b6111085d97f1d950ab0703cc665a1263
Merevoli-DatLuu <datluu.1702@gmail.com>
2021-10-22 17:12:00 +0700

    [Add]: add CORS Allow

--- backend/travel_management/travel_management/settings.py
+++ backend/travel_management/travel_management/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = 'django-insecure-6fh00@$bca#i-t7_s^ch+mh&84wpgk^ppah%h3=vj!t!^)@+wh
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Application definition
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'base.apps.BaseConfig',
     'tour.apps.TourConfig',
 ]
@@ -46,6 +48,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
