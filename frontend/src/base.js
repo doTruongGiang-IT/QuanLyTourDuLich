7fd7538994d22808f3688426588178457821dac2
doTruongGiang-IT <dotruonggiang290@gmail.com>
2021-10-24 16:32:12 +0700

    [Modified]: api url

--- frontend/src/base.js
+++ frontend/src/base.js
@@ -4,7 +4,7 @@ export const get_api_url = () => {
     let API_PORT = 8000;
     let FRONT_END_PORT = 3000;
     let substrPoint = 8 + FRONT_END_PORT.toString().length;
-    if(domainWithOutGitpod.substring(0, substrPoint).includes("https://3000") && domainWithOutGitpod.substring(substrPoint, domainWithOutGitpod.length).includes("gitpod.io")) {
+    if(domainWithOutGitpod.substring(0, substrPoint).includes(`https://${FRONT_END_PORT}`) && domainWithOutGitpod.substring(substrPoint, domainWithOutGitpod.length).includes("gitpod.io")) {
       result = `${domainWithOutGitpod.substring(0, 8)}${API_PORT}${domainWithOutGitpod.substring(substrPoint, domainWithOutGitpod.length)}`;
     }else {
       result = `http://localhost:${API_PORT}/`;
