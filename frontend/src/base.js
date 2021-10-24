43a2ca8cefe770f492c6816915842c3f06629a66
doTruongGiang-IT <dotruonggiang290@gmail.com>
2021-10-24 16:29:06 +0700

    [Modified]: Frontend port

--- frontend/src/base.js
+++ frontend/src/base.js
@@ -2,7 +2,7 @@ export const get_api_url = () => {
     let result = "";
     let domainWithOutGitpod = window.location.href;
     let API_PORT = 8000;
-    let FRONT_END_PORT = window.location.port;
+    let FRONT_END_PORT = 3000;
     let substrPoint = 8 + FRONT_END_PORT.toString().length;
     if(domainWithOutGitpod.substring(0, substrPoint).includes("https://3000") && domainWithOutGitpod.substring(substrPoint, domainWithOutGitpod.length).includes("gitpod.io")) {
       result = `${domainWithOutGitpod.substring(0, 8)}${API_PORT}${domainWithOutGitpod.substring(substrPoint, domainWithOutGitpod.length)}`;
