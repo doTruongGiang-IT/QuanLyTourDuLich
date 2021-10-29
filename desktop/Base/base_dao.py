0b8ac1f80da92ac508c87ada3011f99fb0bf4d93
Merevoli-DatLuu <47155364+Merevoli-DatLuu@users.noreply.github.com>
2021-10-30 01:49:22 +0700

    [Refactor]: base_bus & base_dao

--- desktop/Base/base_dao.py
+++ desktop/Base/base_dao.py
@@ -1,4 +1,5 @@
 import requests
+from Base.error import Error
 
 
 API_HOST = 'http://localhost:8000'
@@ -16,17 +17,37 @@ class BaseDAO:
     }
     
     def to_create_request_data(self, data: DTO_CLASS) -> dict:
+        """
+        Convert DTO_CLASS to request's data when using post method
+        This method need to be overrided
+        :data: DTO_CLASS -> DTO_CLASS instance
+        :return: dict -> request's data
+        """
         pass
     
     def to_update_request_data(self, data: DTO_CLASS) -> dict:
+        """
+        Convert DTO_CLASS to request's data when using post method
+        :data: DTO_CLASS -> DTO_CLASS instance
+        :return: dict -> request's data
+        """
         request_data = self.to_create_request_data(data)
         request_data['id'] = data.id
         return request_data
     
-    def get_object(self, data: dict):
+    def get_object(self, data: dict) -> DTO_CLASS:
+        """
+        Convert data to DTO_CLASS instance
+        :data: dict
+        :return: DTO_CLASS
+        """
         return self.DTO_CLASS(**data)
     
-    def read(self) -> tuple[str, list[DTO_CLASS]]:
+    def read(self) -> tuple[Error, list[DTO_CLASS] | None]:
+        """
+        Read method in DAO, equivalent to GET method (get list action)
+        :return: tuple[Error, list[DTO_CLASS] | None]
+        """
         request = requests.get(self.API_URL['read'])
     
         if request.status_code == 200:
@@ -36,43 +57,63 @@ class BaseDAO:
             for data in response['results']:
                 result.append(self.get_object(data))
                 
-            return ('', result)
+            return (Error(False, None), result)
         else:
-            return ('Get the tour list that have an error', None)
+            return (Error(True, 'Get the tour list that have an error'), None)
     
-    def read_detail(self, tour_id: int) -> tuple[str, DTO_CLASS]:
+    def read_detail(self, tour_id: int) -> tuple[Error, DTO_CLASS | None]:
+        """
+        Read detail method in DAO, equivalent to GET method (get detail action)
+        :tour_id: int -> Tour id
+        :return: tuple[Error, DTO_CLASS | None]
+        """
         request = requests.get(self.API_URL['read_detail'].format(tour_id))
     
         if request.status_code == 200:
             response = request.json()
             result = self.get_object(response)
                 
-            return ('', result)
+            return (Error(False, None), result)
         else:
-            return ('Get the tour that have an error', None)
+            return (Error(True, 'Get the tour that have an error'), None)
     
-    def create(self, data: DTO_CLASS) -> str:
+    def create(self, data: DTO_CLASS) -> Error:
+        """
+        Create method in DAO, equivalent to POST method (create action)
+        :data: DTO_CLASS -> DTO_CLASS instance
+        :return: Error
+        """
         request_data = self.to_create_request_data(data)
         request = requests.post(self.API_URL['create'], data=request_data)
         
         if request.status_code == 201:
-            return ''
+            return Error(False, None)
         else:
-            return 'create new tour that have an error'
+            return Error(True, 'create new tour that have an error')
         
-    def update(self, data: DTO_CLASS) -> str:
+    def update(self, data: DTO_CLASS) -> Error:
+        """
+        Update method in DAO, equivalent to PATCH method (partial_update action)
+        :data: DTO_CLASS -> DTO_CLASS instance
+        :return: Error
+        """
         request_data = self.to_update_request_data(data)
         request = requests.patch(self.API_URL['update'].format(data.id), data=request_data)
         
         if request.status_code == 200:
-            return ''
+            return Error(False, None)
         else:
-            return 'update a tour that have an error'
+            return Error(True, 'update a tour that have an error')
         
-    def delete(self, tour_id: int) -> str:
+    def delete(self, tour_id: int) -> Error:
+        """
+        Delete method in DAO, equivalent to delete method (destroy action)
+        :tour_id: int -> Tour id
+        :return: Error
+        """
         request = requests.delete(self.API_URL['delete'].format(tour_id))
         
         if request.status_code == 204:
-            return ''
+            return Error(False, None)
         else:
-            return 'delete a tour that have an error'
+            return Error(True, 'delete a tour that have an error')
