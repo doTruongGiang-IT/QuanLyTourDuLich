a4779dfb2038818f17c0f40e72b3ae2b11f631de
doTruongGiang-IT <dotruonggiang290@gmail.com>
2021-10-30 10:19:09 +0700

    [Add]: data to location table

--- frontend/src/components/TourLocationForm/TourLocationForm.js
+++ frontend/src/components/TourLocationForm/TourLocationForm.js
@@ -1,78 +1,34 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import 'antd/dist/antd.css';
 import React, {useState} from 'react';
-import { Table, Input, Button, Space, Popconfirm, Form, notification} from 'antd';
-import { SearchOutlined, DeleteOutlined, EditOutlined, EyeOutlined, BookOutlined } from '@ant-design/icons';
-import EditableCell from '../EditableCell/EditableCell';
-import "./TourListForm.css";
-import { Link } from 'react-router-dom';
-import { useHistory } from 'react-router';
+import { Table, Input, Button, Space} from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
+import './TourLocationForm.css';
 
-const TourLocationForm = () => {
-    let tourList = [];
+const TourLocationForm = ({locations}) => {
+    let results = [];
+    let formatLocations = locations ? locations.map(location => {
+        return [location.journey];
+    }) : [];
 
+    formatLocations.forEach(locations => {
+        locations.forEach(location => {
+            location.forEach(locationName => {
+                if(locationName.location.type !== "Hotel" && locationName.location.type !== "Unknown") {
+                    if(!results.includes(locationName.location.name)) {
+                        results.push(locationName.location.name);
+                    };
+                };
+            });
+        });
+    });
+
+    results = results.map((result, index) => {
+        return {key: index, stt: index+1, name: result, type: "Tourist Area"};
+    });
+    
     const [searchText, setSearchText] = useState("");
     const [searchedColumn, setSearchedColumn] = useState("");
-    const [form] = Form.useForm();
-    const [editData, setEditData] = useState([]);
-    const [editingKey, setEditingKey] = useState('');
-    const [selectedRowKeys, setSelectedKeys] = useState([]);
-    const [tourLocations, setTourLocations] = useState("");
-    const [api, contextHolder] = notification.useNotification();
-    const Context = React.createContext();
-    const history = useHistory();
-
-    const onSelectChange = (selectedRowKeys) => {
-        setSelectedKeys(selectedRowKeys);
-    };
-
-    const rowSelection = {
-        selectedRowKeys,
-        onChange: onSelectChange,
-        selections: [
-          Table.SELECTION_ALL,
-          Table.SELECTION_INVERT,
-          Table.SELECTION_NONE,
-          {
-            key: 'odd',
-            text: 'Select Odd Row',
-            onSelect: changableRowKeys => {
-                let newSelectedRowKeys = [];
-                newSelectedRowKeys = changableRowKeys.filter((key, index) => {
-                    if (index % 2 !== 0) {
-                        return false;
-                    };
-                    return true;
-                });
-                setSelectedKeys(newSelectedRowKeys);
-            },
-          },
-          {
-            key: 'even',
-            text: 'Select Even Row',
-            onSelect: changableRowKeys => {
-                let newSelectedRowKeys = [];
-                newSelectedRowKeys = changableRowKeys.filter((key, index) => {
-                    if (index % 2 !== 0) {
-                        return true;
-                    };
-                    return false;
-                });
-                setSelectedKeys(newSelectedRowKeys);
-            },
-          },
-          {
-            key: 'delete',
-            text: 'Delete Selected Rows',
-            onSelect: () => {
-                if(selectedRowKeys.length > 0) {
-                    selectedRowKeys.forEach(async (selectedRowKey) => await handleDelete(selectedRowKey-1));
-                };
-                setSelectedKeys([]);
-            },
-          },
-        ],
-    };
 
     const getColumnSearchProps = dataIndex => ({
         filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
@@ -130,81 +86,18 @@ const TourLocationForm = () => {
         setSearchText({ searchText: '' });
     };
 
-    const handleDelete = (id) => {
-        // setDataInfo(data.filter((item) => item.key !== key));
-        // remove(id);
-        openNotification("Deleted");
-    };
-
-    const isEditing = (record) => record.key === editingKey;
-
-    const edit = (record) => {
-        form.setFieldsValue({
-        ...record,
-        });
-        setEditingKey(record.key);
-    };
-
-    const cancel = () => {
-        setEditingKey('');
-    };
-
-    const saveEdit = async (id) => {
-        try {
-          const row = await form.validateFields();
-          const newData = [...tourList];
-          const index = newData.findIndex((item) => id === item.id);
-    
-          if (index > -1) {
-            const item = newData[index];
-            newData.splice(index, 1, { ...item, ...row });
-            setEditData(newData);
-            // update(newData[index]);
-            // setDataInfo(newData);
-            setEditingKey('');
-            openNotification("Update");
-          } else {
-            newData.push(row);
-            setEditData(newData);
-            setEditingKey('');
-          }
-        } catch (errInfo) {
-          console.log('Validate Failed:', errInfo);
-        }
-    };
-
-    const quickSort = (values) => {
-        if (values.length <= 1) {
-            return values
-        };
-
-        var lessThanPivot = [];
-        var greaterThanPivot = [];
-        var pivot = values[0];
-        for (var i = 1; i < values.length; i++) {
-            if (values[i].id <= pivot.id) {
-                lessThanPivot.push(values[i]);
-            } else {
-                greaterThanPivot.push(values[i]);
-            }
-        }
-        return quickSort(lessThanPivot).concat(pivot, quickSort(greaterThanPivot));
-    };
-
     const columns = [
         {
             title: 'STT',
             dataIndex: 'stt',
             key: 'stt',
             width: '10%',
-            ...getColumnSearchProps('stt'),
         },
         {
             title: 'Name',
             dataIndex: 'name',
             key: 'name',
-            width: '30%',
-            editable: true,
+            width: '50%',
             ...getColumnSearchProps('name'),
             sorter: (a, b) => a.name.length - b.name.length,
             sortDirections: ['descend', 'ascend'],
@@ -213,88 +106,13 @@ const TourLocationForm = () => {
             title: 'Type',
             dataIndex: 'type',
             key: 'type',
-            width: '20%',
-            editable: true,
-            ...getColumnSearchProps('type'),
-        },
-        {
-            title: 'Actions',
-            width: '10%',
-            render: (_, record) => {
-                const editable = isEditing(record);
-                return editable ? (
-                    <Space>
-                        <Button
-                            onClick={() => saveEdit(record.id)}
-                            type="primary"
-                            size="small"
-                            style={{ width: 60 }}
-                        >
-                            Save
-                        </Button>
-                        <Popconfirm title="Sure to cancel?" onConfirm={cancel}>
-                            <Button
-                                type="primary"
-                                size="small"
-                                style={{ width: 60 }}
-                                danger
-                            >
-                                Cancel
-                            </Button>
-                        </Popconfirm>
-                    </Space> 
-                ) : (
-                    <Space>
-                        <Popconfirm title="Sure to delete?" onConfirm={() => handleDelete(record.id)}>
-                            <Button
-                                type="primary"
-                                icon={<DeleteOutlined />}
-                                size="small"
-                                style={{ width: 40 }}
-                                danger
-                            />
-                        </Popconfirm>
-                        <Button
-                            disabled={editingKey !== ''} 
-                            onClick={() => edit(record)}
-                            type="primary"
-                            icon={<EditOutlined />}
-                            size="small"
-                            style={{ width: 40 }}
-                        />
-                    </Space>   
-                )
-            },
         },
     ];
 
-    const mergedColumns = columns.map((col) => {
-        if (!col.editable) {
-          return col;
-        }
-    
-        return {
-          ...col,
-          onCell: (record) => ({
-            record,
-            inputType: 'text',
-            dataIndex: col.dataIndex,
-            title: col.title,
-            editing: isEditing(record),
-          }),
-        };
-    });
-
-    const openNotification = placement => {
-        api.info({
-            message: `Notification`,
-            description: <Context.Consumer>{() => `${placement} Success!`}</Context.Consumer>,
-        });
-    };
-
     return (
-        <div>
-            
+        <div className="tourLocationsForm">
+            <h2>Locations for this tour</h2>
+            <Table bordered columns={columns} dataSource={results} pagination={{defaultPageSize: 20}} scroll={{ y: 300}} />
         </div>
     )
 }
