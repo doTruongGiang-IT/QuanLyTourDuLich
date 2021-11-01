9311b0e54b0c9eb304771a020e1bf5c914944db5
Kiều Đặng Kim Bảo <kieudangkimbao2000@gmail.com>
2021-11-01 13:34:48 +0700

    [Add]: code GroupJourneyDAO, GroupJourneyBUS

--- desktop/BUS/group.py
+++ desktop/BUS/group.py
@@ -1,7 +1,13 @@
 from Base.base_bus import BaseBUS
 from DTO.group import Group
 from DAO.group import GroupDAO
+from desktop.DAO.group import GroupJourneyDAO
+from desktop.DTO.group import GroupJourney
 
 class GroupBUS(BaseBUS):
     DTO_CLASS = Group
     DAO_CLASS = GroupDAO
+
+class GroupJourneyBUS(BaseBUS):
+    DTO_CLASS = GroupJourney
+    DAO_CLASS = GroupJourneyDAO
--- desktop/DAO/group.py
+++ desktop/DAO/group.py
@@ -5,7 +5,7 @@ from Base.error import Error
 from DTO.group import Group, GroupJourney
 
 class GroupDAO(BaseDAO):
-    DTO_CLASS = None
+    DTO_CLASS = Group
     API_URL = {
         'read': f'{BASE_API_URL}/group',
         'read_detail': f'{BASE_API_URL}/group/{{}}',
@@ -17,6 +17,7 @@ class GroupDAO(BaseDAO):
     def get_object(self, data):
         data['start_date'] = datetime.strptime(data['start_date'], '%Y-%m-%d')
         data['end_date'] = datetime.strptime(data['end_date'], '%Y-%m-%d')
+        
         return self.DTO_CLASS(**data)
 
     def to_create_request_data(self, data):
@@ -24,7 +25,33 @@ class GroupDAO(BaseDAO):
             'name': data.name,
             'start_date': data.start_date,
             'end_date': data.end_date,
-            'journey': data.joourney
+            'journey': data.journey
+        }
+
+        return request_data
+
+class GroupJourneyDAO(BaseDAO):
+    DTO_CLASS = GroupJourney
+    API_URL = {
+        'read': f'{BASE_API_URL}/group/groupjourney',
+        'read_detail': f'{BASE_API_URL}/group/groupjourney/{{}}',
+        'create': f'{BASE_API_URL}/group/groupjourney',
+        'update': f'{BASE_API_URL}/group/groupjourney/{{}}',
+        'delete': f'{BASE_API_URL}/group/groupjourney/{{}}'
+    }
+
+    def get_object(self, data):
+        data['start_date'] = datetime.strptime(data['start_date'], '%Y-%m-%d')
+        data['end_date'] = datetime.strptime(data['end_date'], '%Y-%m-%d')
+
+        return self.DTO_CLASS(**data)
+
+    def to_create_request_data(self, data):
+        request_data = {
+            'content': data.content,
+            'start_date': data.start_date,
+            'end_date': data.end_date,
+            'location': data.location
         }
 
         return request_data
