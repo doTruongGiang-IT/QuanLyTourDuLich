1f12fdb3c08af95f9d4e705ac66b19a5e4f9c334
Merevoli-DatLuu <47155364+Merevoli-DatLuu@users.noreply.github.com>
2021-11-03 00:44:02 +0700

    [Add]: Location GUI

--- desktop/GUI/pages/tour.py
+++ desktop/GUI/pages/tour.py
@@ -1,6 +1,7 @@
 import dearpygui.dearpygui as dpg
 
 from .tour_tour import TourTourGUI
+from .tour_location import TourLocationGUI
 
 class TourGUI:
     content_window = "content_window"
@@ -10,7 +11,7 @@ class TourGUI:
         ["tour_characteristic_tab_bar_menu_window", "Tour Characteristic", TourTourGUI.content_render],
         ["tour_type_tab_bar_menu_window", "Tour Type", TourTourGUI.content_render],
         ["tour_price_tab_bar_menu_window", "Tour Price", TourTourGUI.content_render],
-        ["location_tab_bar_menu_window", "Location", TourTourGUI.content_render]
+        ["location_tab_bar_menu_window", "Location", TourLocationGUI.content_render]
     ]
     
     @classmethod
@@ -62,4 +63,5 @@ class TourGUI:
     @classmethod
     def location_render_callback(cls, sender, app_data):
         cls.init_content_window()
-        cls.location_render(str(sender))
+        TourLocationGUI.group_content_window = cls.group_content_window
+        TourLocationGUI.content_render(str(sender))
