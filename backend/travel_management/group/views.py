d83f88a179f9b94cf0500f6d581106641806636e
Merevoli-DatLuu <47155364+Merevoli-DatLuu@users.noreply.github.com>
2021-11-03 01:09:19 +0700

    [Modified]: Group, GroupJourney API

--- backend/travel_management/group/views.py
+++ backend/travel_management/group/views.py
@@ -1,13 +1,15 @@
+from base.views import TrackingMixin
 from django.db.models import Q
 from rest_framework import viewsets
 
-from .models import Group
-from .serializers import GroupSerializer
+from .models import Group, GroupJourney
+from .serializers import GroupSerializer, GroupJourneySerializer
 
 
-class GroupViewSet(viewsets.ModelViewSet):
+class GroupViewSet(TrackingMixin, viewsets.ModelViewSet):
     serializer_class = GroupSerializer
     queryset = Group.objects.all()
+    object_name = 'group'
     
     def filter_queryset(self, queryset): 
         tour_id = self.request.query_params.get("tour_id", None)
@@ -20,10 +22,39 @@ class GroupViewSet(viewsets.ModelViewSet):
         return super().filter_queryset(queryset)
     
     
+class GroupJourneyViewSet(TrackingMixin, viewsets.ModelViewSet):
+    serializer_class = GroupJourneySerializer
+    queryset = GroupJourney.objects.all()
+    object_name = 'group_journey'
+    
+    def filter_queryset(self, queryset): 
+        group_id = self.request.query_params.get("group_id", None)
+        query = Q()
+
+        if group_id:
+            query = query & Q(group_id=group_id)
+
+        queryset = queryset.filter(query)
+        return super().filter_queryset(queryset)
+    
 group_list = GroupViewSet.as_view({
-    'get': 'list'
+    'get': 'list',
+    'post': 'create'
 })
 
 group_detail = GroupViewSet.as_view({
-    'get': 'retrieve'
+    'get': 'retrieve',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+
+group_journey_list = GroupJourneyViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+
+group_journey_detail = GroupJourneyViewSet.as_view({
+    'get': 'retrieve',
+    'patch': 'partial_update',
+    'delete': 'destroy'
 })
