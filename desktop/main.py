e20de2c240b34270bf56cd2336803f32d41c3ebc
Merevoli-DatLuu <47155364+Merevoli-DatLuu@users.noreply.github.com>
2021-11-03 19:43:59 +0700

    [Add]: classic_theme, light_theme, gruvbox_theme

--- desktop/main.py
+++ desktop/main.py
@@ -1,10 +1,11 @@
+import json
 import threading
 
 import dearpygui.dearpygui as dpg
 
 from Base.lazy_loading import async_lazy_loading
+from GUI.base_menu import init_onclose, menu_content, menu_menu
 from GUI.base_window import init_font, init_window
-from GUI.base_menu import init_onclose, menu_menu, menu_content
 
 dpg.create_context()
         
@@ -16,12 +17,52 @@ lazy_loading_thread = threading.Thread(target=async_lazy_loading, args=(), daemo
 lazy_loading_thread.start()
             
 dpg.show_style_editor()
-# dpg.show_imgui_demo()
+dpg.show_imgui_demo()
 # dpg.show_debug()
 # dpg.show_item_registry()
 
+
+data = open("./GUI/theme_data.json", "r")
+data = ''.join(data.readlines())
+data = json.loads(data)
+
+
+def init_style():
+    with dpg.theme_component(dpg.mvAll):
+        dpg.add_theme_style(dpg.mvStyleVar_CellPadding, 8, 2)
+
+def set_classic_theme():
+    with dpg.theme(default_theme=True) as classic_theme:
+        with dpg.theme_component(dpg.mvAll):
+            for attr, value in data['classic_theme'].items():
+                dpg.add_theme_color(getattr(dpg, attr), value)
+                
+        init_style() 
+        
+    dpg.bind_theme(classic_theme)
+    
+def set_light_theme():
+    with dpg.theme() as light_theme:
+        with dpg.theme_component(dpg.mvAll):
+            for attr, value in data['light_theme'].items():
+                dpg.add_theme_color(getattr(dpg, attr), value)
+        
+        init_style() 
+    
+    dpg.bind_theme(light_theme)
+    
+def set_gruvbox_theme():
+    with dpg.theme() as gruvbox_theme:
+        with dpg.theme_component(dpg.mvAll):
+            for attr, value in data['gruvbox_theme'].items():
+                dpg.add_theme_color(getattr(dpg, attr), value)
+                
+        init_style() 
+    
+    dpg.bind_theme(gruvbox_theme)
+    
 dpg.configure_app(docking=True, docking_space=True)
-dpg.create_viewport(title='Custom Title', width=1280, height=840)
+dpg.create_viewport(title='Custom Title', width=1280, height=860)
 with dpg.viewport_menu_bar():
     with dpg.menu(label="Window"):
         dpg.add_menu_item(tag="menu_menu_window", label="Show Menu Window", check=True, default_value=True, callback=menu_menu)
@@ -33,19 +74,13 @@ with dpg.viewport_menu_bar():
         dpg.add_menu_item(label="Show Style Editor", callback=lambda:dpg.show_tool(dpg.mvTool_Style))
         dpg.add_menu_item(label="Show Font Manager", callback=lambda:dpg.show_tool(dpg.mvTool_Font))
     with dpg.menu(label="Theme"):
-        dpg.add_menu_item(label="Use Classic theme")
+        dpg.add_menu_item(label="Use Classic theme", callback=set_classic_theme)
         dpg.add_menu_item(label="Use Dark theme")
-        dpg.add_menu_item(label="Use Light theme")
-        dpg.add_menu_item(label="Use Gruvbox theme")
+        dpg.add_menu_item(label="Use Light theme", callback=set_light_theme)
+        dpg.add_menu_item(label="Use Gruvbox theme", callback=set_gruvbox_theme)
         dpg.add_menu_item(label="Use Green theme")  
 
-with dpg.theme() as global_theme:
-
-    with dpg.theme_component(dpg.mvAll):
-        dpg.add_theme_style(dpg.mvStyleVar_CellPadding, 8, 2)
-
-dpg.bind_theme(global_theme)
-
+                
 dpg.setup_dearpygui()
 dpg.show_viewport()
 dpg.start_dearpygui()
