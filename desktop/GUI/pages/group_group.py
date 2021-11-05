a89ef2b356caaa826f210765c3f890d16471232c
Kiều Đặng Kim Bảo <kieudangkimbao2000@gmail.com>
2021-11-05 10:47:06 +0700

    [Add, Modified]: code create, update, delete, view group; delete column journey

--- desktop/GUI/pages/group_group.py
+++ desktop/GUI/pages/group_group.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import date, datetime
 
 import dearpygui.dearpygui as dpg
 from BUS.group import GroupBUS, GroupJourneyBUS
@@ -31,7 +31,7 @@ class GroupGroupGUI:
     def choice_tour_combo(cls, sender, app_data):
         tour_id = int(app_data.split('|')[0])
         
-        header = ['id', 'name', 'start_date', "end_date", "revenue", "journey"]
+        header = ['id', 'name', 'start_date', "end_date", "revenue"]
         datetime_type = lambda d: datetime.strptime(d, '%Y-%m-%d')
         type_columns = [int, str, datetime_type, datetime_type, int, str]
         data = []
@@ -45,8 +45,7 @@ class GroupGroupGUI:
                 d.name,
                 d.start_date.strftime("%Y-%m-%d"),
                 d.end_date.strftime("%Y-%m-%d"),
-                d.revenue,
-                d.journey
+                d.revenue
             ])
         
         if cls.table is not None:
@@ -65,26 +64,23 @@ class GroupGroupGUI:
         
     @classmethod
     def create_window(cls):
-        window = dpg.add_window(label="Add new tour", width=400, autosize=True, pos=[500, 200])
-        tour_name = dpg.add_input_text(label="Name ", parent=window)
-        
-        tour_characteristics    = TourCharacteristicBUS().objects
-        tour_types              = TourTypeBUS().objects
-        tour_prices             = TourPriceBUS().objects
-        locations               = LocationBUS().objects
-        
-        tour_characteristics    = [f'{d.id} | {d.name}' for d in tour_characteristics]
-        tour_types              = [f'{d.id} | {d.name}' for d in tour_types]
-        tour_prices             = [f'{d.id} | {d.name}' for d in tour_prices]
-        locations               = [f'{d.id} | {d.name}' for d in locations]
-        
-        tour_characteristics    = dpg.add_combo(label="Characteristic", items=tour_characteristics, parent=window)
-        tour_types              = dpg.add_combo(label="Type", items=tour_types, parent=window)
-        tour_prices             = dpg.add_combo(label="Price", items=tour_prices, parent=window)
-        locations               = dpg.add_combo(label="Location", items=locations, parent=window)
+        window = dpg.add_window(label="Add new group", width=400, autosize=True, pos=[500, 200])
+        group_name = dpg.add_input_text(label="Name ", parent=window)
+
+        tours = [f'{t.id} | {t.name}' for t in TourBUS().objects]
+        group_tour = dpg.add_combo(label="Tour", items=tours, parent=window)
+
+        datenow = {
+            'month_day': date.today().day,
+            'year': date.today().year-1970+70,
+            'month': date.today().month-1
+        }
+        group_start_date = dpg.add_date_picker(label="Start Date ", parent=window, default_value=datenow)
+        group_end_date = dpg.add_date_picker(label="End Date ", parent=window, default_value=datenow)
+        group_revenue = dpg.add_input_text(label="Revenue ", parent=window)
         
         group = dpg.add_group(horizontal=True, parent=window)
-        button = dpg.add_button(label="Add new tour", callback=cls.create_window_callback, parent=group)
+        button = dpg.add_button(label="Add new group", callback=cls.create_window_callback, parent=group)
         status = dpg.add_text(default_value="Status", parent=group)
         dpg.set_item_user_data(
             button, 
@@ -94,28 +90,28 @@ class GroupGroupGUI:
                 'items': [
                     {
                         'field': 'name',
-                        'name': 'Tour name',
-                        'item': tour_name,
+                        'name': 'Group name',
+                        'item': group_name,
                     },
                     {
-                        'field': 'characteristic',
-                        'name': 'Tour characteristic',
-                        'item': tour_characteristics,
+                        'field': 'tour',
+                        'name': 'Tour',
+                        'item': group_tour,
                     },
                     {
-                        'field': 'type',
-                        'name': 'Tour type',
-                        'item': tour_types,
+                        'field': 'start_date',
+                        'name': 'Start date',
+                        'item': group_start_date,
                     },
                     {
-                        'field': 'price',
-                        'name': 'Tour price',
-                        'item': tour_prices,
+                        'field': 'end_date',
+                        'name': 'End date',
+                        'item': group_end_date,
                     },
                     {
-                        'field': 'location',
-                        'name': 'Location',
-                        'item': locations,
+                        'field': 'revenue',
+                        'name': 'Revenue',
+                        'item': group_revenue,
                     }
                 ]
             }
@@ -140,89 +136,95 @@ class GroupGroupGUI:
             dpg.configure_item(user_data['status'], default_value=f'Status: OK', color=[128, 237, 153])
             print(request_data)
             
-            request_data['characteristic']  = int(request_data['characteristic'].split('|')[0])        
-            request_data['type']            = int(request_data['type'].split('|')[0])        
-            request_data['price']           = int(request_data['price'].split('|')[0])        
-            request_data['location']        = int(request_data['location'].split('|')[0])    
+            request_data['tour']  = int(request_data['tour'].split('|')[0])
+
+            start_date = datetime(request_data['start_date']['year']-70+1970, request_data['start_date']['month']+1, request_data['start_date']['month_day'])
+            end_date = datetime(request_data['end_date']['year']-70+1970, request_data['end_date']['month']+1, request_data['end_date']['month_day'])
+
+            request_data['revenue'] = int(request_data['revenue'])
             
-            print(request_data)
-            
-            tour_obj = Tour(
-                id=0,
-                name=request_data['name'],
-                characteristic=TourCharacteristic(request_data['characteristic'], None),
-                type=TourType(request_data['type'], None),
-                price=TourPrice(request_data['price'], None, None, None, None),
-                location=Location(request_data['location'], None, None, None)
-            ) 
-            tour_bus = TourBUS()
-            error = tour_bus.create(tour_obj)
+            group_obj = Group(
+                id = 0,
+                name = request_data['name'],
+                tour = request_data['tour'],
+                start_date = start_date,
+                end_date = end_date,
+                revenue = request_data['revenue'],
+                journey = []
+            )
+
+            group_bus = GroupBUS()
+            error = group_bus.create(group_obj)
             
             if error.status is True:
                 dpg.configure_item(user_data['status'], default_value=f'Status: {error.message}', color=[255, 92, 88])
             else:
                 dpg.delete_item(user_data['window'])
-                cls.content_render("tour")
+                cls.content_render("group")
                 
     @classmethod
     def modified_window(cls, sender, app_data, user_data):
-        tours = TourBUS().objects
+        groups = GroupBUS().objects
         
-        tour = [t for t in tours if t.id == user_data][0]
+        group_object = [g for g in groups if g.id == user_data][0]
         
-        window = dpg.add_window(label="Modified the tour", width=400, autosize=True, pos=[500, 200])
-        tour_id = dpg.add_text(default_value=f"id: {tour.id}", parent=window)
-        tour_name = dpg.add_input_text(label="Name ", parent=window, default_value=tour.name)
-        
-        tour_characteristics    = TourCharacteristicBUS().objects
-        tour_types              = TourTypeBUS().objects
-        tour_prices             = TourPriceBUS().objects
-        locations               = LocationBUS().objects
-        
-        tour_characteristics    = [f'{d.id} | {d.name}' for d in tour_characteristics]
-        tour_types              = [f'{d.id} | {d.name}' for d in tour_types]
-        tour_prices             = [f'{d.id} | {d.name}' for d in tour_prices]
-        locations               = [f'{d.id} | {d.name}' for d in locations]
-        
-        tour_characteristics    = dpg.add_combo(label="Characteristic", items=tour_characteristics, default_value=f'{tour.characteristic.id} | {tour.characteristic.name}', parent=window)
-        tour_types              = dpg.add_combo(label="Type", items=tour_types, default_value=f'{tour.type.id} | {tour.type.name}', parent=window)
-        tour_prices             = dpg.add_combo(label="Price", items=tour_prices, default_value=f'{tour.price.id} | {tour.price.name}', parent=window)
-        locations               = dpg.add_combo(label="Location", items=locations, default_value=f'{tour.location.id} | {tour.location.name}', parent=window)
+        window = dpg.add_window(label="Modified the group", width=400, autosize=True, pos=[500, 200])
+
+        group_id = dpg.add_text(default_value=f"id: {group_object.id}", parent=window)
+        group_name = dpg.add_input_text(label="Name ", parent=window, default_value=group_object.name)
+
+        tours = [f'{t.id} | {t.name}' for t in TourBUS().objects]
+        tour = [f'{t.id} | {t.name}' for t in TourBUS().objects if t.id == group_object.tour][0]
+        group_tour = dpg.add_combo(label="Tour", items=tours, parent=window, default_value=tour)
+
+        start_date={
+            'month_day': group_object.start_date.day,
+            'year': group_object.start_date.year-1970+70,
+            'month': group_object.start_date.month-1
+        }
+        end_date={
+            'month_day': group_object.end_date.day,
+            'year': group_object.end_date.year-1970+70,
+            'month': group_object.end_date.month-1
+        }
+        group_start_date = dpg.add_date_picker(label="Start Date ", parent=window, default_value=start_date)
+        group_end_date = dpg.add_date_picker(label="End Date ", parent=window, default_value=end_date)
+        group_revenue = dpg.add_input_text(label="Revenue ", parent=window, default_value=group_object.revenue)
         
         group = dpg.add_group(horizontal=True, parent=window)
-        button = dpg.add_button(label="Save the tour", callback=cls.modified_window_callback, parent=group)
+        button = dpg.add_button(label="Save the group", callback=cls.modified_window_callback, parent=group)
         status = dpg.add_text(default_value="Status", parent=group)
         dpg.set_item_user_data(
             button, 
             {
                 'window': window,
                 'status': status, 
-                'id': tour.id,
+                'id': group_object.id,
                 'items': [
                     {
                         'field': 'name',
-                        'name': 'Tour name',
-                        'item': tour_name,
+                        'name': 'Group name',
+                        'item': group_name,
                     },
                     {
-                        'field': 'characteristic',
-                        'name': 'Tour characteristic',
-                        'item': tour_characteristics,
+                        'field': 'tour',
+                        'name': 'Tour',
+                        'item': group_tour,
                     },
                     {
-                        'field': 'type',
-                        'name': 'Tour type',
-                        'item': tour_types,
+                        'field': 'start_date',
+                        'name': 'Start date',
+                        'item': group_start_date,
                     },
                     {
-                        'field': 'price',
-                        'name': 'Tour price',
-                        'item': tour_prices,
+                        'field': 'end_date',
+                        'name': 'End date',
+                        'item': group_end_date,
                     },
                     {
-                        'field': 'location',
-                        'name': 'Location',
-                        'item': locations,
+                        'field': 'revenue',
+                        'name': 'Revenue',
+                        'item': group_revenue,
                     }
                 ]
             }
@@ -247,42 +249,44 @@ class GroupGroupGUI:
             dpg.configure_item(user_data['status'], default_value=f'Status: OK', color=[128, 237, 153])
             print(request_data)
             
-            request_data['characteristic']  = int(request_data['characteristic'].split('|')[0])        
-            request_data['type']            = int(request_data['type'].split('|')[0])        
-            request_data['price']           = int(request_data['price'].split('|')[0])        
-            request_data['location']        = int(request_data['location'].split('|')[0])    
+            request_data['tour']  = int(request_data['tour'].split('|')[0])
+
+            start_date = datetime(request_data['start_date']['year']-70+1970, request_data['start_date']['month']+1, request_data['start_date']['month_day'])
+            end_date = datetime(request_data['end_date']['year']-70+1970, request_data['end_date']['month']+1, request_data['end_date']['month_day'])
+
+            request_data['revenue'] = int(request_data['revenue'])
             
-            print(request_data)
-            
-            tour_obj = Tour(
-                id=user_data['id'],
-                name=request_data['name'],
-                characteristic=TourCharacteristic(request_data['characteristic'], None),
-                type=TourType(request_data['type'], None),
-                price=TourPrice(request_data['price'], None, None, None, None),
-                location=Location(request_data['location'], None, None, None)
-            ) 
-            tour_bus = TourBUS()
-            error = tour_bus.update(tour_obj)
+            group_obj = Group(
+                id = user_data['id'],
+                name = request_data['name'],
+                tour = request_data['tour'],
+                start_date = start_date,
+                end_date = end_date,
+                revenue = request_data['revenue'],
+                journey = []
+            )
+
+            group_bus = GroupBUS()
+            error = group_bus.update(group_obj)
             
             if error.status is True:
                 dpg.configure_item(user_data['status'], default_value=f'Status: {error.message}', color=[255, 92, 88])
             else:
                 dpg.delete_item(user_data['window'])
-                cls.content_render('tour')
+                cls.content_render("group")
         
     @classmethod
     def delete_window(cls, sender, app_data, user_data):
-        tour_id = user_data
+        group_id = user_data
         window = dpg.add_window(label="Modified the tour", width=400, autosize=True, pos=[500, 200])
 
-        question = dpg.add_text(default_value=f"Do you want to delete the tour (id: {tour_id})?", parent=window)
+        question = dpg.add_text(default_value=f"Do you want to delete the group (id: {group_id})?", parent=window)
         status = dpg.add_text(default_value="Status", parent=window)
         
         group = dpg.add_group(horizontal=True, parent=window)
         
         user_data = {
-            'tour_id': tour_id,
+            'group_id': group_id,
             'status': status,
             'window': window
         }
@@ -291,24 +295,31 @@ class GroupGroupGUI:
         
     @classmethod
     def delete_window_callback(cls, sender, app_data, user_data):
-        error = TourBUS().delete(user_data['tour_id'])
+        error = GroupBUS().delete(user_data['group_id'])
         if error.status is True:
             dpg.configure_item(user_data['status'], default_value=f'Status: {error.message}', color=[255, 92, 88])
         else:
             dpg.configure_item(user_data['status'], default_value=f'Status: OK', color=[128, 237, 153])
             dpg.delete_item(user_data['window'])
-            cls.content_render('tour')
+            cls.content_render('group')
             
     @classmethod
     def view_window(cls, sender, app_data, user_data):
-        tours = TourBUS().objects
-        tour = [t for t in tours if t.id == user_data][0]
+        group = [g for g in GroupBUS().objects if g.id == user_data][0]
+
+        tour = [f'{t.id} | {t.name}' for t in TourBUS().objects if t.id == group.tour][0]
         
         window = dpg.add_window(label="Modified the tour", width=400, autosize=True, pos=[500, 200])
-        dpg.add_text(default_value=f"id: {tour.id}", parent=window)
-        dpg.add_text(default_value=f"Name: {tour.name}", parent=window)
-        dpg.add_text(default_value=f"Characteristic: {tour.characteristic.id} | {tour.characteristic.name}", parent=window)
-        dpg.add_text(default_value=f"Type: {tour.type.id} | {tour.type.name}", parent=window)
-        dpg.add_text(default_value=f"Price: {tour.price.id} | {tour.price.name}", parent=window)
-        dpg.add_text(default_value=f"Location: {tour.location.id} | {tour.location.name}", parent=window)
+        dpg.add_text(default_value=f"id: {group.id}", parent=window)
+        dpg.add_text(default_value=f"Name: {group.name}", parent=window)
+        dpg.add_text(default_value=f"Tour: {tour}", parent=window)
+        dpg.add_text(default_value=f"Start date: {group.start_date}", parent=window)
+        dpg.add_text(default_value=f"End date: {group.end_date}", parent=window)
+        journey = ""
+        for j in group.journey:
+                start_hour = str(j.start_date.hour) + " h " + str(j.start_date.minute)
+                end_hour = str(j.end_date.hour) + " h " + str(j.end_date.minute)
+                journey +=  start_hour + " - " + end_hour + ": " + j.content + "\n"
+        dpg.add_text(default_value=f"Journey: {journey}", parent=window)
+        dpg.add_text(default_value=f"Revenue: {group.revenue}", parent=window)
         dpg.add_button(label="Close", callback=lambda :dpg.delete_item(window), parent=window)
