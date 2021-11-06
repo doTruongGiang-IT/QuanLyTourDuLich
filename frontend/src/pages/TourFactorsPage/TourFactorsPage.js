1a07fe62e6e72305f0b77242b318c908d8a3d670
doTruongGiang-IT <dotruonggiang290@gmail.com>
2021-11-06 14:53:46 +0700

    [Add]: Code tour location screen

--- frontend/src/pages/TourFactorsPage/TourFactorsPage.js
+++ frontend/src/pages/TourFactorsPage/TourFactorsPage.js
@@ -1,19 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
 import TourCharacteristic from '../../components/TourCharacteristic/TourCharacteristic';
+import TourLocation from '../../components/TourLocation/TourLocation';
 import TourPrice from '../../components/TourPrice/TourPrice';
 import TourType from '../../components/TourType/TourType';
 import './TourFactorsPage.css';
 
 const TourFactorPage = () => {
+    const[isVisible, setIsVisible] = useState(false);
+
+    const handleVisible = () => {
+        setIsVisible(!isVisible);
+    };
+
     return (
         <div className="tourFactorsPage">
             <div className="tourItem">
                 <TourCharacteristic />
                 <TourType />
             </div>
-            <div className="tourItem">
-                <TourPrice />
-            </div>
+            {
+                !isVisible ?
+                <p id="showMore" onClick={handleVisible}>Show more...</p> : 
+                (
+                    <>
+                        <div id="moreItem">
+                            <div className="tourItem">
+                                <TourPrice />
+                            </div>
+                            <div className="tourItem">
+                                <TourLocation />
+                            </div>
+                        </div>
+                        <p id="showMore" onClick={handleVisible}>Show less...</p>
+                    </>
+                )
+            }
         </div>
 
     )
