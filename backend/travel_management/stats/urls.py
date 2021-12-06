69a939a168dc2e06d13cbdd196a97360b8cf104d
Merevoli-DatLuu <47155364+Merevoli-DatLuu@users.noreply.github.com>
2021-12-06 22:20:05 +0700

    [Add]: Cost and Revenue of groups

--- backend/travel_management/stats/urls.py
+++ backend/travel_management/stats/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import stats_tour_staff_detail
+from .views import stats_tour_staff_list, stats_cost_revenue_group_list
 
 urlpatterns = [
-    path('tour_of_staff',       stats_tour_staff_detail,        name='stats-tour-staff-detail'),
+    path('tour_of_staff',                       stats_tour_staff_list,                  name='stats-tour-staff-list'),
+    path('stats_cost_revenue_group_list/<int:pk>',       stats_cost_revenue_group_list,          name='stats-cost-revenue-group-list'),
 ]
